0f6651180c7ad294a43a84a797a69acc61565d46
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-12-03 00:36:04 -0500

    Added plugin template code

---
+++ src/pasta/CityCraft.java
@@ -0,0 +1,16 @@
+package pasta;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CityCraft extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
