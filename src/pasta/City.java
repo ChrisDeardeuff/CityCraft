a92fa80a857a2123e4462dafa387ad3501a88eeb
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-12-03 23:02:23 -0500

    Added fields to City.java

--- src/pasta/City.java
+++ src/pasta/City.java
@@ -1,81 +1,82 @@
 package pasta;
 
+import com.google.gson.Gson;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class City
-{
+public class City {
 
     private String cityName;
-    private Player mayor;
+    private String mayor;
     private ChunkHashTable ownedLand;
     private Location TPP;
     ArrayList<Player> residents;
 
+    public City(String cityName, String mayor) {
+        this.cityName = cityName;
+        this.mayor = mayor;
 
-    public String getCityName()
-    {
+        ownedLand = new ChunkHashTable();
+    }
+
+    public void saveAsJSON() throws IOException {
+
+        Gson gson = new Gson();
+
+        gson.toJson(this, new FileWriter("./data.json"));
+    }
+
+    public String getCityName() {
         return cityName;
     }
 
-    public void setCityName(String cityName)
-    {
+    public void setCityName(String cityName) {
         this.cityName = cityName;
     }
 
-    public Player getMayor()
-    {
+    public String getMayor() {
         return mayor;
     }
 
-    public void setMayor(Player mayor)
-    {
+    public void setMayor(String mayor) {
         this.mayor = mayor;
     }
 
-    public ChunkHashTable getOwnedLand()
-    {
+    public ChunkHashTable getOwnedLand() {
         return ownedLand;
     }
 
-    public void setOwnedLand(ChunkHashTable ownedLand)
-    {
+    public void setOwnedLand(ChunkHashTable ownedLand) {
         this.ownedLand = ownedLand;
     }
 
-    public Location getTPP()
-    {
+    public Location getTPP() {
         return TPP;
     }
 
-    public void setTPP(Location TPP)
-    {
+    public void setTPP(Location TPP) {
         this.TPP = TPP;
     }
 
-    public ArrayList<Player> getResidents()
-    {
+    public ArrayList<Player> getResidents() {
         return residents;
     }
 
-    public void setResidents(ArrayList<Player> residents)
-    {
+    public void setResidents(ArrayList<Player> residents) {
         this.residents = residents;
     }
 
-
-    class ChunkHashTable
-    {
+    class ChunkHashTable{
 
         ArrayList[] harray = new ArrayList[500];
 
-        public ChunkHashTable()
-        {
-            for (int i = 0; i < 500; i++)
-            {
+        public ChunkHashTable(){
+            for (int i = 0; i < 500; i++) {
 
                 harray[i] = new ArrayList<Chunk>();
             }
@@ -84,11 +85,9 @@ public class City
 
         /**
          * Insert Chunk c into hashtable based on key.
-         *
          * @param c Chunk to insert.
          */
-        public void insert(Chunk c)
-        {
+        public void insert(Chunk c){
 
             int key = hashFunction(c);
 
@@ -98,11 +97,9 @@ public class City
 
         /**
          * Removes Chunk from hash table
-         *
          * @param c Chunk to be removed
          */
-        public void remove(Chunk c)
-        {
+        public void remove(Chunk c){
 
             harray[hashFunction(c)].remove(c);
 
@@ -110,26 +107,21 @@ public class City
 
         /**
          * Searches hash table for Chunk c.
-         *
          * @param c Chunk to look for
          * @return true if chunk is in table, false if not.
          */
-        public boolean find(Chunk c)
-        {
+        public boolean find(Chunk c){
             int key = hashFunction(c);
 
             return harray[key].contains(c);
         }
-
         /**
          * Returns value of key for Chunk c.
          * Key is calculated by adding the Chunks z and x coords and modding by 500 (size of hash table).
-         *
          * @param c chunk to find key for.
          * @return key of Chunk c.
          */
-        public int hashFunction(Chunk c)
-        {
+        public int hashFunction(Chunk c){
 
             int key = (c.getX() + c.getZ()) % 500;
 
@@ -137,6 +129,5 @@ public class City
         }
 
     }
-
 
 }
